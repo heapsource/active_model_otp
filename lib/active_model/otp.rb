95d058751a97823a4afd17cf3b16d975667e1a2a
robertomiranda <rjmaltamar@gmail.com>
2013-04-06 09:45:06 -0500

    Custom column name in has_one_time_password

--- lib/active_model/otp.rb
+++ lib/active_model/otp.rb
@@ -4,13 +4,18 @@ module ActiveModel
 
     module ClassMethods
       def has_one_time_password(options = {})
+
+        cattr_accessor :otp_column_name
+
+        self.otp_column_name = (options[:column_name] || "otp_secret_key").to_s
+
         include InstanceMethodsOnActivation
 
         before_create { self.otp_secret_key = ROTP::Base32.random_base32 }
 
         if respond_to?(:attributes_protected_by_default)
           def self.attributes_protected_by_default #:nodoc:
-            super + ['otp_secret_key']
+            super + [self.otp_column_name]
           end
         end
       end
@@ -18,12 +23,17 @@ module ActiveModel
 
     module InstanceMethodsOnActivation
       def authenticate_otp(code)
-        ROTP::TOTP.new(self.otp_secret_key).verify(code)
+        ROTP::TOTP.new(self.otp_column).verify(code)
       end
 
       def provisioning_uri
-        ROTP::TOTP.new(self.otp_secret_key).provisioning_url(self.email)
+        ROTP::TOTP.new(self.otp_column).provisioning_url(self.email)
       end
+
+      def otp_column
+        self.send(self.class.otp_secret_key)
+      end
+
     end
   end
 end
