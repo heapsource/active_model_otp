19b520654eff2554c2bfdfa7bec5ee078afd2221
hjinu <hjw7789@gmail.com>
2015-01-08 22:53:56 +0900

    add test user model

--- test/models/guest.rb
+++ test/models/guest.rb
@@ -1,11 +1,11 @@
-class Visitor
+class Guest
   extend ActiveModel::Callbacks
   include ActiveModel::Validations
   include ActiveModel::OneTimePassword
 
   define_model_callbacks :create
-  attr_accessor :otp_token, :email
+  attr_accessor :otp_secret_key, :email
 
-  has_one_time_password column_name: :otp_token, length: 4
+  has_one_time_password interval: 60
 end
 
