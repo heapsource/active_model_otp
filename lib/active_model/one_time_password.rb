03d822e9a4f1618f471c2eb83109796554c65b0b
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-06-18 20:04:25 -0300

    Update one_time_password.rb

--- lib/active_model/one_time_password.rb
+++ lib/active_model/one_time_password.rb
@@ -7,7 +7,7 @@ module ActiveModel
         cattr_accessor :otp_column_name, :otp_counter_column_name,
                        :otp_backup_codes_column_name
         class_attribute :otp_digits, :otp_counter_based,
-                        :otp_backup_codes_count, :otp_one_time_backup_codes, 
+                        :otp_backup_codes_count, :otp_one_time_backup_codes,
                         :otp_interval
 
         self.otp_column_name = (options[:column_name] || "otp_secret_key").to_s
